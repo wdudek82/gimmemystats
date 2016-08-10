8386b55ebf55bdce20372bf700c26fe434951120
Wojciech Dudek <wdudek82@gmail.com>
2016-08-10 22:33:39 +0200

    First attemt to format columns width

--- gimmemystats_xlsx.py
+++ gimmemystats_xlsx.py
@@ -25,38 +25,45 @@ def gimme_nao():
 		cur.execute(qs)
 
 		# col_style = xlwt.easyxf('font: bold on')
-		filename = 'report-{}_{}.xlsx'.format(table_name, report_date_str)
+		filename = '{}_{}.xlsx'.format(table_name, report_date_str)
 
 		wb = xlsxwriter.Workbook(filename)
 		ws = wb.add_worksheet(table_name)
+
+		# workbook style
+		bold = wb.add_format({'bold': True})
 
 		# Creating column headers
 		colnames = [desc[0] for desc in cur.description]
 
 		for colnum, colname in enumerate(colnames):
-			ws.write(0, colnum, colname) #, col_style)
+			ws.set_column(colnum, colnum, len(str(colname)))
+			ws.write(0, colnum, colname, bold)
 
 		# Extracting data from the DB, and then 
 		# writing it to spreadsheet
 		rows = cur.fetchall()
 
 		count_rows = 0
+		date_format = wb.add_format({'num_format': 'dd/mm/yyyy'})
 		for rownum, row in enumerate(rows):
 
+			longest_cell = 10
 			for colnum, cell in enumerate(row):
 
 				if type(cell) is datetime:
-					ws.write(rownum+1, colnum, str(cell)[:19])
+					# ws.write(rownum+1, colnum, str(cell)[:19])
+					ws.write_datetime(rownum+1, colnum, cell, date_format)
 				else:
 					ws.write(rownum+1, colnum, cell)
 
+
 				# TODO: Adjusting column width to widest data found
+				if ws.col_sizes.get(colnum) < len(str(cell)):
+					ws.set_column(colnum, colnum, len(str(cell)))
+
 
 			count_rows += 1
-
-			# if count_rows == 65535:
-			# 	print('[Error] Too many freaking rows in {}! Repors truncated!\n'.format(table_name))
-			# 	break
 		
 		print('-' * 60)
 		print('-> report: {}'.format(filename))
